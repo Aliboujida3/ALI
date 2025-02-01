9702e9af8386b1fc05d7223dd4a333951b3f9292
Aliboujida3 <147938931+Aliboujida3@users.noreply.github.com>
2025-02-01 23:20:05 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,6 @@
-(function() {
-    emailjs.init("ChNzpVN8Zi0TD2yAU");
-})();
-
-function sendFollowerRequest() {
-    var username = document.getElementById("username").value;
-
-    if (username) {
-        emailjs.send("Service_diavolo12", "template_009c0je", {
-            username: username
-        }).then(function(response) {
-            alert("تم إرسال الطلب بنجاح!");
-            document.getElementById("followerForm").reset();
-        }, function(error) {
-            alert("فشل في إرسال الطلب، حاول مرة أخرى.");
-        });
-    } else {
-        alert("يرجى إدخال اسم المستخدم.");
+function updateUsername() {
+    let username = document.getElementById("accountInput").value;
+    if (username.trim() !== "") {
+        document.getElementById("username").textContent = username;
     }
 }
